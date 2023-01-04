22f37eae1a4124f470845dce881acc9b062262d1
NonoTheAwesome <112124931+NonoTheAwesome@users.noreply.github.com>
2023-01-04 23:28:57 +0200

    Add files via upload

---
+++ rlm.js
@@ -0,0 +1,5 @@
+var video = document.getElementById('traffic-video');
+
+video.addEventListener('play', function() {
+  // Update the page with additional information about the traffic officers
+});
